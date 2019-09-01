d2efe1b3ed71d70dba0fbdf2c87f7ffc89f47067
itzezc <chirub@foretag.co>
2019-09-01 19:33:03 +0100

    JSX : Base rules

---
+++ source/rules/jsx.js
@@ -0,0 +1,17 @@
+module.exports.rules = {
+
+	'react/jsx-boolean-value': [
+		'error',
+		'never'
+	],
+
+	'react/jsx-child-element-spacing': 'warn',
+
+	'react/jsx-closing-bracket-location': [
+		'warn'
+		'line-aligned',
+	],
+
+	'react/jsx-closing-tag-location': 'warn'
+
+}
