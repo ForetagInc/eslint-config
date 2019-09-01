505c6acfde2392f6a74713c3e1cdc1a6ebaff8cf
itzezc <chirub@foretag.co>
2019-09-01 02:59:44 +0100

    ESLint Babel parser

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+{
+	"parser": "babel-eslint",
+	"parserOptions":  {
+		"sourceType": "module",
+		"allowImportExportEverywhere": false,
+		"codeFrame": true
+	}
+	"plugins": [
+		"react",
+		"react-native"
+	]
+}
