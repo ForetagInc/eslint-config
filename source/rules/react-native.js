623a1d6ed3a1d9439575aee234edcbe521988d5d
itzezc <chirub@foretag.co>
2019-09-01 19:19:44 +0100

    React Native complete rule set

---
+++ source/rules/react-native.js
@@ -0,0 +1,17 @@
+module.exports.rules = {
+	
+	'react-native/no-unused-styles': 'warn',
+
+	'react-native/sort-styles': [
+		'error',
+		'asc'
+	],
+
+	'react-native/split-platform-components': 'error',
+
+	'react-native/no-inline-styles': 'error',
+
+	'react-native/no-color-literals': 'error',
+
+	'react-native/no-raw-text': 'error'
+}
