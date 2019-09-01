228c8fe1ca7ad2906d4b0636283ecc314113627d
itzezc <chirub@foretag.co>
2019-09-01 18:27:05 +0100

    React : Component sorting, optimizations  and more

--- source/rules/react.js
+++ source/rules/react.js
@@ -40,5 +40,64 @@ module.exports.rules = {
 
 	'react/prop-types': 'off',
 
-		
+	'react/react-in-jsx-scope': 'error',
+
+	'react/require-default-props': 'error',
+
+	'react/require-optimization': [ 
+		'error', {
+			'allowDecorators': [
+				'pureRender'
+			]
+		}
+	],
+
+	'react/require-render-return': 'error',
+
+	'react/self-closing-comp': [
+		'error', {
+			'component': true,
+			'html': true
+		}
+	],
+
+	'react/sort-comp': [
+		'warn', {
+			order: [
+				'static-methods',
+				'lifecycle',
+				'everything-else',
+				'render'
+			],
+			groups: {
+				lifecycle: [
+				'displayName',
+				'propTypes',
+				'contextTypes',
+				'childContextTypes',
+				'mixins',
+				'statics',
+				'defaultProps',
+				'constructor',
+				'getDefaultProps',
+				'state',
+				'getInitialState',
+				'getChildContext',
+				'getDerivedStateFromProps',
+				'componentWillMount',
+				'UNSAFE_componentWillMount',
+				'componentDidMount',
+				'componentWillReceiveProps',
+				'UNSAFE_componentWillReceiveProps',
+				'shouldComponentUpdate',
+				'componentWillUpdate',
+				'UNSAFE_componentWillUpdate',
+				'getSnapshotBeforeUpdate',
+				'componentDidUpdate',
+				'componentDidCatch',
+				'componentWillUnmount'
+				]
+			}
+		}
+	]
 }
