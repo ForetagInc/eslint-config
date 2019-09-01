9cbaea0385c291783b6218d3d1067655c3d7ef24
itzezc <chirub@foretag.co>
2019-09-01 14:15:10 +0100

    Babel base linting

---
+++ rules/babel.js
@@ -0,0 +1,19 @@
+module.exports.rules = {
+
+	'babel/new-cap': 'error',
+
+	'babel/camelcase': 'warn',
+
+	'babel/no-invalid-this': 'error',
+
+	'babel/object-curly-spacing': 'error',
+
+	'babel/quotes': 'off',
+
+	'babel/semi': 'off',
+
+	'babel/no-unused-expressions': 'off',
+
+	'babel/valid-typeof': 'off'
+
+}
