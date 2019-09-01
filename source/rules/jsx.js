25c120e73fc85bb0bec8fd7991f90110ed17ac02
itzezc <chirub@foretag.co>
2019-09-01 21:12:03 +0100

    JSX : curly new line and spacing

--- source/rules/jsx.js
+++ source/rules/jsx.js
@@ -12,6 +12,24 @@ module.exports.rules = {
 		'line-aligned',
 	],
 
-	'react/jsx-closing-tag-location': 'warn'
+	'react/jsx-closing-tag-location': 'warn',
+
+	'react/jsx-curly-newline': [
+		'warn', {
+			'multiline': 'consistent',
+			'singleline': 'consistent'
+		}
+	],
+
+	'react/jsx-curly-spacing': [
+		'warn', {
+			'when': 'always',
+			'children': true,
+			'allowMultiline': true,
+			'spacing': {
+				'objectLiterals': 'never'
+			}
+		}
+	],
 
 }
