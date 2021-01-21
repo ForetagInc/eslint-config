08f1cb2ce92c2bea8d67f4e15b1d231a263337bb
itzezc <chirub@foretag.co>
2021-01-21 12:53:12 +0000

    GraphQL parserOptions

--- graphql.js
+++ graphql.js
@@ -9,5 +9,28 @@ module.exports = {
 	],
 	plugins: [
 		'@graphql-eslint',
+	],
+	overrides: [
+		{
+			files: [
+				'*.tsx',
+				'*.ts'
+			],
+			processor: '@graphql-eslint/graphql',
+		},
+		{
+			files: [
+				'*.graphql',
+				'*gql'
+			],
+			plugins: [
+				'@graphql-eslint'
+			],
+			parser: '@graphql-eslint/eslint-graphql',
+			parserOptions: {
+				operations: './source/**/*.graphql',
+				schema: './graphql/schema.graphql'
+			}
+		}
 	]
 }
