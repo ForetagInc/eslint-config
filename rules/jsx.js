e2fc51f1c2d1bc9e645a43bf69b35f14c596b747
itzezc <chirub@foretag.co>
2021-01-21 13:28:01 +0000

    [Rules] JSX

--- rules/jsx.js
+++ rules/jsx.js
@@ -31,5 +31,42 @@ module.exports = {
 				}
 			}
 		],
+
+		'react/jsx-equals-spacing': ['error', 'never'],
+		'react/jsx-filename-extension': ['warn', { extensions: [ '.jsx', '.tsx' ] }],
+		'react/jsx-first-prop-new-line': 'never',
+		'react/jsx-fragments': ['warn', 'syntax'],
+		'react/jsx-handler-names': ['off'],
+		'react/jsx-indent':  ['warn', 'tab', {
+			checkAttributes: true,
+			indentLogicalExpressions: true
+		}],
+		'react/jsx-indent-props': ['warn', 'tab'],
+		'react/jsx-max-depth': ['off'],
+		'react/jsx-max-props-per-line': ['warn', { when: 'multiline' }],
+		'react/jsx-newline': ['off'],
+		'react/jsx-no-bind': ['warn'],
+		'react/jsx-no-comment-textnodes': ['error'],
+		'react/jsx-no-constructed-context-values': ['error'],
+		'react/jsx-no-duplicate-props': ['error'],
+		'react/jsx-no-literals': ['off'],
+		'react/jsx-no-script-url': ['error'],
+		'react/jsx-no-target-blank': ['error'],
+		'react/jsx-no-undef': ['error'],
+		'react/jsx-no-useless-fragment': ['error'],
+		'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
+		'react/jsx-pascal-case': ['error'],
+		'react/jsx-props-no-spreading': ['off'],
+		'react/jsx-sort-default-props': ['off'],
+		'react/jsx-sort-props': ['off'],
+		'react/jsx-space-before-closing': ['error', 'always'],
+		'react/jsx-tag-spacing': ['error', {
+			closingSlash: 'never',
+			beforeSelfClosing: 'always',
+			afterOpening: 'never',
+			beforeClosing: 'never'
+		}],
+		'react/jsx-uses-react': ['error'],
+		'react/jsx-uses-vars': ['error'],
 	}
 }
